f1d40a652fdbea7ab7387c1007613d45a863bec9
Catherine Chowdhury <luna2soriano@gmail.com>
2026-04-25 20:30:54 -0400

    Updated api in Moodtracker.vue

--- client/fitness-tracker/src/views/MoodTracker.vue
+++ client/fitness-tracker/src/views/MoodTracker.vue
@@ -1,17 +1,20 @@
 <script setup lang="ts">
 import { ref, watch } from 'vue'
-
+import { api } from '@/services/myFetch'
 import MoodList from '@/components/MoodList.vue'
 import MoodUpdate from '@/components/MoodUpdate.vue'
 
-const toggleMoodUpdate = ref(false)
+import type { Mood } from '@/types/workout'
+import type { DataListEnvelope } from '@/types/dataEnvelopes'
 
+const toggleMoodUpdate = ref(false)
 const activeUserId = ref(1)
-const moods = ref([])
+
+const moods = ref<Mood[]>([])
 
 async function loadMoods(id: number) {
-  const res = await fetch(`http://localhost:3000/moods/${id}`)
-  moods.value = await res.json()
+  const res = await api<DataListEnvelope<Mood>>(`/moods/${id}`)
+  moods.value = res.data
 }
 
 watch(activeUserId, loadMoods, { immediate: true })
@@ -21,7 +24,11 @@ watch(activeUserId, loadMoods, { immediate: true })
   <div class="container">
     <button class="button is-primary" @click="toggleMoodUpdate = true">Add Mood</button>
 
-    <MoodUpdate v-if="toggleMoodUpdate" @close="toggleMoodUpdate = false" />
+    <MoodUpdate
+      v-if="toggleMoodUpdate"
+      @close="toggleMoodUpdate = false"
+      @saved="loadMoods(activeUserId)"
+    />
 
     <MoodList :moods="moods" />
   </div>
